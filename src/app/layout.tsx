246a8b1d5fa93a1bafb3a5acdd8899ed46ffbfd6
Javier Haro <javihll@gmail.com>
2025-07-29 07:45:21 +0000

    corregir la web y optimizarla: te paso parte del informe : Insights Rend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,20 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Space+Grotesk:wght@400;500;700&display=swap"
+          as="style"
+          onLoad={
+            "this.onload=null;this.rel='stylesheet'"
+          }
+        />
+        <noscript>
+          <link
+            href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Space+Grotesk:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </noscript>
       </head>
       <body className="font-body antialiased">
         <div className="relative flex min-h-screen flex-col bg-background">
